69de2c129564d2820a7e3ad3321e637900d38b45
Zorex Salvo <zsalvo@ayannah.com>
2017-04-18 20:29:44 +0800

    Initial commit on Todo list using VueJS

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <todo-list v-bind:todos="todos"></todo-list>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
